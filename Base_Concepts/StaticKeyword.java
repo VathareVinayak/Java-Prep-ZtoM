fc4e61a4af9d839e3cab8881f657f223ed25d176
VathareVinayak <vinayak.vathare2004@gmail.com>
2025-03-10 12:03:31 +0530

    Revision - Base Concepts

---
+++ Base_Concepts/StaticKeyword.java
@@ -0,0 +1,14 @@
+package Base_Concepts;
+
+public class StaticKeyword { 
+    // Static method
+    static void myStaticMethod() {
+        System.out.println("Static methods can be called without creating objects");
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        myStaticMethod(); 
+        // Calling the static method directly
+    }
+}
